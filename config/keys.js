226278c9933f9565382fa8f78774c254ab8b8133
anhtubui1204 <s3634162@rmit.edu.vn>
2020-03-06 16:44:47 +0700

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports={
-    mongoURI: 'mongodb://healthcare:healthcare123456@ds137508.mlab.com:37508/healthcare',
+    mongoURI: 'mongodb://healthcare:healthcare123456@ds137508.mlab.com:37508/healthcare'
+    // mongoURI: 'mongodb+srv://anhtubui1204:Rmit2019.@lene-0vcth.mongodb.net/SGHForm?retryWrites=true&w=majority'
 };
